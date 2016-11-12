384d6eccbea612b2fb4dcc1eb79ffaea571157ca
AJTrowell <ajtrowell@gmail.com>
2016-11-12 00:47:31 -0600

    Added EncoderNavigation. Updated outputs.
    EncoderNavigation class is verbatim from the current Android FTC
    code.

--- opMode.java
+++ opMode.java
@@ -10,17 +10,23 @@ public class opMode
     public Hardware_iRads robot = new Hardware_iRads();
     public ElapsedTime runtime = new ElapsedTime();
     public Telemetry telemetry = new Telemetry();
+    public EncoderNavigation encoderNav = new EncoderNavigation();
 
     public opMode() {
         runtime.reset(); // Normally this would run inside runOpMode
+        encoderNav.initialize(robot,runtime,telemetry);
     }
 
 
     public void runOpMode() {
 
         telemetry.addData("Status","Running");
-        telemetry.addData("robotWidth",Double.toString(robot.WHEELBASE_WIDTH_IN));
-        telemetry.addData("time",String.format("%.3f",runtime.time()));
+        telemetry.addData("robotWidth",String.format("%.1f mm",robot.WHEELBASE_WIDTH_MM));
+        telemetry.addData("time",String.format("%.3f",runtime.time()) + " seconds\n");
+        
+        encoderNav.setSteps(-700,700);
+        encoderNav.printResults();
+        telemetry.addData("","\n");
 
     }
 
