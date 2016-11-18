60e3ff82143be5061bfbe26a7c76319b595f2ce3
AJTrowell <ajtrowell@gmail.com>
2016-11-18 16:20:56 -0600

    Add Simulation.main() to quickly execute test.
    To run, just right click Simulation in BlueJ, and run 
    main().

---
+++ Simulation.java
@@ -0,0 +1,17 @@
+
+/**
+ * Simulate Opmode running. Run static main.
+ * 
+ * @author Mr. Trowell
+ * @version (11/18/2016)
+ */
+public class Simulation
+{
+
+    public static void main() {
+        opMode op = new opMode();
+        op.runOpMode();
+        
+        
+    }
+}
